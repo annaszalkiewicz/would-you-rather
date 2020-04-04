37801382ef7f5c1cf25fd04c14d068304b4b3054
annaszalkiewicz <anna@mywebgraphicdesign.com>
2020-04-04 23:38:32 +0200

    Set up a project

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Would you rather</p>
+    </div>
+  );
+}
+
+export default App;
