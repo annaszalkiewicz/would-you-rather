931ce9a5822454e3087a1595558d8e093e8824c3
annaszalkiewicz <anna@mywebgraphicdesign.com>
2020-04-17 22:15:08 +0200

    Add leaderboard feature
    Display leaderboard with the score from highest to lowest

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -5,20 +5,63 @@ import './Leaderboard.scss';
 import Header from '../Header/Header';
 
 class Leaderboard extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <h1>Leaderboard component</h1>
-      </div>
-    );
-  }
+	state = {
+		leaderboard: [],
+	};
+	componentDidMount = () => {
+		const { users } = this.props;
+		const leaderboard = Object.values(users)
+			.map((user) => {
+				return {
+					id: user.id,
+					name: user.name,
+					avatar: user.avatarURL,
+					answers: Object.keys(user.answers).length,
+					questions: user.questions.length,
+					score: Object.keys(user.answers).length + user.questions.length,
+				};
+			})
+			.sort((a, b) => b.score - a.score);
+
+		this.setState({ leaderboard: leaderboard });
+	};
+
+	render() {
+		const { leaderboard } = this.state;
+		return (
+			<div>
+				<Header />
+				<main className='leaderboard'>
+					{leaderboard.map((user) => {
+						return (
+							<div className='leaderboard-card'>
+								<div className='leaderboard-left'>
+									<div className='leaderboard-card'>
+										<img src={user.avatar} alt={user.name} />
+									</div>
+								</div>
+								<div className='leaderboard-middle'>
+									<h2>{user.name}</h2>
+									<p>Questions answered: {user.answers}</p>
+									<p>Questions created:{user.questions} </p>
+								</div>
+								<div className='leaderboard-right'>
+									<p>Score:</p>
+									<p>{user.score}</p>
+								</div>
+							</div>
+						);
+					})}
+				</main>
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-  return {
-    users: state.users
-  }
-}
+const mapStateToProps = (state) => {
+	return {
+		users: state.users,
+	};
+};
 
 export default withRouter(connect(mapStateToProps, null)(Leaderboard));
