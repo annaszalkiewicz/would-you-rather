133b97d9a6f2cb6c898ad98cfedf56648e9ddabc
annaszalkiewicz <anna@mywebgraphicdesign.com>
2020-04-18 16:04:52 +0200

    Display voting form when opening unaswered question
    Add question card including form, listen to handleChange event and on submitNewQuestion

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import Home from '../Home/Home';
 import Dashboard from '../Dashboard/Dashboard';
--- src/components/QuestionDetails/QuestionDetails.js
+++ src/components/QuestionDetails/QuestionDetails.js
@@ -5,43 +5,97 @@ import { connect } from 'react-redux';
 import Header from '../Header/Header';
 
 class QuestionDetails extends Component {
+	state = {
+		value: null,
+	};
 
-  state = {
-    currentQuestion: {}
-  }
+	handleChange = (e) => {
+		this.setState({ value: e.target.value });
+	};
 
-  componentDidMount = () => {
-    const questionID = window.location.pathname.slice(11);
-    const currentQuestion = Object.values(this.props.questions).filter(question => {
-      return question.id === questionID;
-    })
-    console.log(questionID);
-    console.log(typeof(currentQuestion) );
-    
-    
-  }
+	submitNewQuestion = (e) => {
+		console.log(this.state.value);
+		e.preventDefault();
+	};
 
-  render() {
-    return (
-      <>
-        <Header />
-        {this.props.isAnswered && (
-          <div>This question has been answered</div>
-        )}
-        {!this.props.isAnswered && (
-          <div>This question has not been answered</div>
-        )}
-      </>
-    );
-  }
+	render() {
+		const questionID = window.location.pathname.slice(11);
+		const currentQuestion = Object.values(this.props.questions).filter(
+			(question) => {
+				return question.id === questionID;
+			}
+		);
+		const currentAuthor = Object.values(this.props.users).filter(
+			(user) => currentQuestion[0].author === user.id
+		);
+
+		return (
+			<>
+				<Header />
+				{!this.props.isAnswered && (
+					<div className='question'>
+						<div className='question-heading'>
+							<h2>{currentAuthor[0].name} asks:</h2>
+						</div>
+						<div className='question-details'>
+							<div className='question-details-left'>
+								<img
+									src={currentAuthor[0].avatarURL}
+									alt={currentAuthor[0].name}
+								/>
+							</div>
+							<div className='question-details-right'>
+								<h3>Would you rather</h3>
+								<form
+									className='question-form'
+									onSubmit={this.submitNewQuestion}
+								>
+									<div className='question-form-row'>
+										<input
+											type='radio'
+											name='optionOneText'
+											id='optionOneText'
+											value='optionOne'
+											checked={this.state.value === 'optionOne'}
+											onChange={this.handleChange}
+										/>
+										<label htmlFor='optionOneText'>
+											{currentQuestion[0].optionOne.text}
+										</label>
+									</div>
+									<div className='question-form-row'>
+										<input
+											type='radio'
+											name='optionOneText'
+											id='optionOneText'
+											value='optionTwo'
+											checked={this.state.value === 'optionTwo'}
+											onChange={this.handleChange}
+										/>
+										<label htmlFor='optionOneText'>
+											{currentQuestion[0].optionTwo.text}
+										</label>
+									</div>
+									<div className='question-form-row'>
+										<input type='submit' value='Vote' />
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				)}
+				{this.props.isAnswered && <div>This question has been answered</div>}
+			</>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-  return {
-    auth: state.auth.authUser,
-    questions: state.questions,
-    users: state.users,
-    isAnswered: state.data.isAnswered
-  }
-}
+const mapStateToProps = (state) => {
+	return {
+		auth: state.auth.authUser,
+		questions: state.questions,
+		users: state.users,
+		isAnswered: state.data.isAnswered,
+	};
+};
 export default withRouter(connect(mapStateToProps, null)(QuestionDetails));
