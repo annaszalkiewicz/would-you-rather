a063bf6dfbeaad38d339d1c4c33aed27e95f03f4
annaszalkiewicz <anna@mywebgraphicdesign.com>
2020-04-22 00:56:20 +0200

    Write instructions how to apply enhancers to store
    Enhancers allow us to debug store with Redux DevTools

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,31 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers/rootReducer';
 
-// Configure store with Redux Dev Tools
-const enhancers =
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// CONFIGURE STORE WITH REDUX DEVTOOLS
 
-const configureStore = () => {
-	return createStore(rootReducer, compose(applyMiddleware(thunk), enhancers));
-};
+/* If you would like to use app with Redux Dev Tools
+   1) Install extension in your browser:
+			For Chrome: https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd
+			For Mozilla: https://addons.mozilla.org/en-US/firefox/addon/reduxdevtools/?src=search
+	 2) Uncomment variable enancers
+	 3) Add compose to import from 'redux'
+	 3) Uncomment configureStore() that includes enhancers
+	 4) Comment configureStore() without enhancers
+*/
 
-//Configure store without Redux Dev Tools
+// const enhancers =
+// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 // const configureStore = () => {
-// 	return createStore(rootReducer, applyMiddleware(thunk));
+// 	return createStore(rootReducer, compose(applyMiddleware(thunk), enhancers));
 // };
+
+// Configure store without Redux Dev Tools
+
+const configureStore = () => {
+	return createStore(rootReducer, applyMiddleware(thunk));
+};
 
 export default configureStore;
