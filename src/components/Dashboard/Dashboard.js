a53ef38579eb1572a721faffa9149ea55459d0d9
annaszalkiewicz <anna@mywebgraphicdesign.com>
2020-04-22 22:43:16 +0200

    Add messages if user has no answered or unaswered question

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import Header from '../Header/Header';
 import Question from '../Question/Question';
 
 import './Dashboard.scss';
+import PrimaryButton from '../ui/PrimaryButton/PrimaryButton';
 
 class Dashboard extends Component {
+	state = {
+		selectedIndex: 0,
+	};
+
+	handleSelect = (index) => {
+		this.setState({ selectedIndex: index });
+	};
+
+	handleClick = () => {
+		this.setState({ selectedIndex: 0 });
+	};
+	
 	render() {
 		const cardYellow = {
 			borderTop: 'solid 5px #1BC495',
@@ -37,12 +50,37 @@ class Dashboard extends Component {
 			<div id='dashbboard'>
 				<Header />
 				<main className='tabs-container' id='container'>
-					<Tabs>
+					<Tabs
+						selectedIndex={this.state.selectedIndex}
+						onSelect={this.handleSelect}
+					>
 						<TabList>
 							<Tab>Unanswered Questions</Tab>
 							<Tab>Answered Questions</Tab>
 						</TabList>
 						<TabPanel>
+							{unanswered.length === 0 && (
+								<div className='no-more-unanswered'>
+									<h2 className='no-more-answers'>
+										Congratulations! You have answered to all question
+									</h2>
+									<p>Create more polls and answer to new questions!</p>
+									<Link to='/add'>
+										<PrimaryButton>Create new poll</PrimaryButton>
+									</Link>
+								</div>
+							)}
+							{unanswered.length === 0 && (
+								<div className='no-more-unanswered'>
+									<h2 className='no-more-answers'>
+										Congratulations! You have answered to all question
+									</h2>
+									<p>Create more polls and answer to new questions!</p>
+									<Link to='/add'>
+										<PrimaryButton>Create new poll</PrimaryButton>
+									</Link>
+								</div>
+							)}
 							{unanswered.map((question) => {
 								return (
 									<Question
@@ -55,6 +93,17 @@ class Dashboard extends Component {
 							})}
 						</TabPanel>
 						<TabPanel>
+							{answered.length === 0 && (
+								<div className='no-more-unanswered'>
+									<h2 className='no-more-answers'>
+										You haven't answered to any poll yet :(
+									</h2>
+									<p>Start answering to questions and earn higher score!</p>
+									<PrimaryButton onClick={this.handleClick}>
+										Answer Questions
+									</PrimaryButton>
+								</div>
+							)}
 							{answered.map((question) => {
 								return (
 									<Question
--- src/data/_DATA.js
+++ src/data/_DATA.js
@@ -35,6 +35,13 @@ let users = {
       "6ni6ok3ym7mf1p33lnez": 'optionTwo'
     },
     questions: ['6ni6ok3ym7mf1p33lnez', 'xj352vofupe1dqz9emx13r'],
+  },
+  anna: {
+    id: 'anna',
+    name: 'Anna Sz.',
+    avatarURL: sarahImg,
+    answers: {},
+    questions: []
   }
 }
 
